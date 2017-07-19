8050fd7e130dc8276e7409d5cd59358a13fd9c5b
Shireen Garcia <garciasal@gmail.com>
2017-07-19 16:55:16 -0700

    Mostly work on the JS file

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ let req = new XMLHttpRequest();
 request.open('GET', '/https://api.github.com/users/AgentShir');
 request.send();
 
-function reqListener () {
-  console.log(this.responseText);
+// let req = new XMLHttpRequest();
+// req.open("GET", "https://api.github.com/");
+// req.addEventListener("load", reqListener);
+// req.send();
+
+// function reqListener () {
+//   console.log(this.responseText);
+// }
+
+// Look carefully at the function
+fuction displayCharacters () {
+  let data = JSON.parse(this.responseText);
+  let list = ' ';
 }
 
-let req = new XMLHttpRequest();
-req.open("GET", "https://api.github.com/");
-req.addEventListener("load", reqListener);
-req.send();
+// Redo below
+data.results.forEach (function(person) {
+  list += `<li>${person.name}</li>`;
+});
