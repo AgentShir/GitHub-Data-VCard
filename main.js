b3f98fa2141c327857356f9769b0c1d1c89b8e00
Shireen Garcia <garciasal@gmail.com>
2017-07-21 11:56:57 -0700

    Template literals at work literally

--- main.js
+++ main.js
@@ -32,3 +32,23 @@ data.results.forEach (function(person) {
 //   data.results.forEach( function (person) {
 //     list += `<li>${person.name}</li>`;
 //   });
+
+// htmlString +=
+// `<div class="customer">
+//   <img src="${item.picture.large}">
+//     <h2>${item.name.first} ${item.name.last}</h2>
+//       <p>${item.email}</p>
+//       <p>${item.location.street}</p>
+//       <p>${item.location.city}, ${item.location.state} ${item.location.postcode}</p>
+//       <p>${item.phone}</p>
+//       <p>${item.id.value}</p>
+// </div>`
+// })
+// htmlString += `</div>`
+
+// Header <h1>${data.name}</h1>
+// Two column layout
+// The Basics
+// login, html_url, company, blog
+// The Story
+//
